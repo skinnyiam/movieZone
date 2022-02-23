dbfb2ecf802841a47dba89eecb3b7d8e73ada120
Saurabh <ss6408367@gmail.com>
2022-02-23 16:50:48 +0530

    fixed api call

--- src/App.js
+++ src/App.js
@@ -6,66 +6,77 @@ import Results from './components/Results'
 import Popup from './components/Popup'
 
 function App() {
-  const [state, setState] = useState({
-    s: "",
-    results: [],
-    selected: {}
-  });
-  const apiurl = "http://www.omdbapi.com/?apikey=a046ebb0";
+    const [state, setState] = useState({
+        s: "",
+        results: [],
+        selected: {}
+    });
+    const apiurl = "https://www.omdbapi.com/?apikey=a046ebb0";
 
-  const search = (e) => {
-    if (e.key === "Enter") {
-      axios(apiurl + "&s=" + state.s).then(({ data }) => {
-        console.log(data);
-        let results = data.Search;
+    const search = (e) => {
+        if (e.key === "Enter") {
+            axios(apiurl + "&s=" + state.s).then(({ data }) => {
+                console.log(data);
+                let results = data.Search;
+
+                setState(prevState => {
+                    return {...prevState, results: results }
+                })
+            });
+        }
+    }
+
+    const handleInput = (e) => {
+        let s = e.target.value;
 
         setState(prevState => {
-          return { ...prevState, results: results }
-        })
-      });
+            return {...prevState, s: s }
+        });
     }
-  }
-  
-  const handleInput = (e) => {
-    let s = e.target.value;
 
-    setState(prevState => {
-      return { ...prevState, s: s }
-    });
-  }
+    const openPopup = id => {
+        axios(apiurl + "&i=" + id).then(({ data }) => {
+            let result = data;
 
-  const openPopup = id => {
-    axios(apiurl + "&i=" + id).then(({ data }) => {
-      let result = data;
+            console.log(result);
 
-      console.log(result);
+            setState(prevState => {
+                return {...prevState, selected: result }
+            });
+        });
+    }
 
-      setState(prevState => {
-        return { ...prevState, selected: result }
-      });
-    });
-  }
+    const closePopup = () => {
+        setState(prevState => {
+            return {...prevState, selected: {} }
+        });
+    }
 
-  const closePopup = () => {
-    setState(prevState => {
-      return { ...prevState, selected: {} }
-    });
-  }
+    return ( <
+        div className = "App" >
+        <
+        header >
+        <
+        h1 > Movie Database < /h1> <
+        /header> <
+        main >
+        <
+        Search handleInput = { handleInput }
+        search = { search }
+        />
 
-  return (
-    <div className="App">
-      <header>
-        <h1>Movie Database</h1>
-      </header>
-      <main>
-        <Search handleInput={handleInput} search={search} />
+        <
+        Results results = { state.results }
+        openPopup = { openPopup }
+        />
 
-        <Results results={state.results} openPopup={openPopup} />
+        {
+            (typeof state.selected.Title != "undefined") ? < Popup selected = { state.selected }
+            closePopup = { closePopup }
+            /> : false} <
+            /main> <
+            /div>
+        );
+    }
 
-        {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup={closePopup} /> : false}
-      </main>
-    </div>
-  );
-}
-
-export default App
+    export default App
